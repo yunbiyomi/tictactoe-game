1ac6731f5cf91321266d605b9b0afb8fe025d882
yunbiyomi <yunbiyomi@naver.com>
2024-03-24 18:19:26 +0900

    ✨ Feat : 무승부 판별 기능 추가

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,20 +1,19 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import styled from 'styled-components'
 import Square from './Square'
 
 const GameBoard = ({ size, currentPlayer, player1Mark, player2Mark, setCurrentPlayer, winCondition }) => {
   const [board, setBoard] = useState(Array(size).fill(Array(size).fill('')));
 
-  // 승리 조건을 검사하는 함수
+  // 승리 조건을 갖추었는지 검사하는 함수
   const checkWin = (row, col) => {
     const markToCheck = currentPlayer === 'player1' ? player1Mark : player2Mark;
 
-    // 가로, 세로, 대각선 방향으로 승리 조건을 검사합니다.
     const directions = [
       [[0, 1], [0, -1]], // 가로
       [[1, 0], [-1, 0]], // 세로
-      [[1, 1], [-1, -1]], // 대각선 (왼쪽 위에서 오른쪽 아래)
-      [[1, -1], [-1, 1]] // 대각선 (오른쪽 위에서 왼쪽 아래)
+      [[1, 1], [-1, -1]], // 오른쪽 위 -> 왼쪽 아래
+      [[1, -1], [-1, 1]] // 왼쪽 위 -> 오른쪽 아래
     ];
 
     for (const direction of directions) {
@@ -31,13 +30,32 @@ const GameBoard = ({ size, currentPlayer, player1Mark, player2Mark, setCurrentPl
         }
       }
 
-      if (count >= winCondition) {
-        return true; // 승리 조건 충족
-      }
+      if (count >= winCondition)
+        return true;
     }
-
+    
     return false;
   };
+
+  // 무승부 확인 해주는 함수
+  const checkDraw = () => {
+    for(let i = 0; i < size; i++) {
+      for(let j = 0; j < size; j++) {
+        if(board[i][j] === '')
+          return false;
+      }
+    }
+    return true;
+  }
+
+  useEffect(() => {
+    if(checkDraw()) {
+      setTimeout(() => {
+        alert(`무승부입니다!`);
+      }, 0);
+      setCurrentPlayer(null);
+    }
+  }, [board])
 
   // 칸 클릭시 플레이어 별 알맞은 마크 표시해주는 함수
   const handleSquareClick = (row, col) => {
@@ -55,13 +73,12 @@ const GameBoard = ({ size, currentPlayer, player1Mark, player2Mark, setCurrentPl
       });
       setBoard(newBoard);
 
-      // 게임 종료 여부 검사
+      // 승리 조건을 갖추었는지 확인
       if (checkWin(row, col)) {
         setTimeout(() => {
           alert(`${currentPlayer}가 승리하였습니다!`);
         }, 0);
         setCurrentPlayer(null);
-        // 게임 종료 후 필요한 작업 수행
       } else {
         setCurrentPlayer(currentPlayer === 'player1' ? 'player2' : 'player1');
       }
@@ -69,7 +86,7 @@ const GameBoard = ({ size, currentPlayer, player1Mark, player2Mark, setCurrentPl
   };
 
   // size의 수만큼 칸을 생성해주는 함수
-  const renderRows = () => {
+  const renderSquares = () => {
     return board.map((rowArr, rowIndex) => (
       <GameBoardRow key={rowIndex}>
         {rowArr.map((val, colIndex) => (
@@ -85,7 +102,7 @@ const GameBoard = ({ size, currentPlayer, player1Mark, player2Mark, setCurrentPl
 
   return (
     <GameBoardContainer>
-      {renderRows()}
+      {renderSquares()}
     </GameBoardContainer>
   )
 }
