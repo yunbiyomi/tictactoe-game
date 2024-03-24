126abe66babb2069f21d08ff787c874f61e2f320
yunbiyomi <yunbiyomi@naver.com>
2024-03-24 17:24:46 +0900

    ✨ Feat : Tic Tac Toe 게임 기능 구현

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,23 +1,86 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components'
 import Square from './Square'
 
-const GameBoard = ({ size }) => {
-  // size수만큼 칸 생성해주는 함수
-  const renderRows = () => {
-    const rows = [];
-    for (let i = 0; i < size; i++) {
-      const rowSquares = [];
-      for (let j = 0; j < size; j++) {
-        rowSquares.push(<Square key={`${i * size + j}`} value=''/>);
+const GameBoard = ({ size, currentPlayer, player1Mark, player2Mark, setCurrentPlayer, winCondition }) => {
+  const [board, setBoard] = useState(Array(size).fill(Array(size).fill('')));
+
+  // 승리 조건을 검사하는 함수
+  const checkWin = (row, col) => {
+    const markToCheck = currentPlayer === 'player1' ? player1Mark : player2Mark;
+
+    // 가로, 세로, 대각선 방향으로 승리 조건을 검사합니다.
+    const directions = [
+      [[0, 1], [0, -1]], // 가로
+      [[1, 0], [-1, 0]], // 세로
+      [[1, 1], [-1, -1]], // 대각선 (왼쪽 위에서 오른쪽 아래)
+      [[1, -1], [-1, 1]] // 대각선 (오른쪽 위에서 왼쪽 아래)
+    ];
+
+    for (const direction of directions) {
+      let count = 1;
+
+      for (const d of direction) {
+        let r = row + d[0];
+        let c = col + d[1];
+
+        while (r >= 0 && r < size && c >= 0 && c < size && board[r][c] === markToCheck) {
+          count++;
+          r += d[0];
+          c += d[1];
+        }
       }
-      rows.push(
-        <GameBoardRow key={i}>
-          {rowSquares}
-        </GameBoardRow>
-      );
+
+      if (count >= winCondition) {
+        return true; // 승리 조건 충족
+      }
     }
-    return rows;
+
+    return false;
+  };
+
+  // 칸 클릭시 플레이어 별 알맞은 마크 표시해주는 함수
+  const handleSquareClick = (row, col) => {
+    if (currentPlayer && board[row][col] === '') {
+      const newBoard = board.map((rowArr, rowIndex) => {
+        if (rowIndex === row) {
+          return rowArr.map((val, colIndex) => {
+            if (colIndex === col) {
+              return currentPlayer === 'player1' ? player1Mark : player2Mark;
+            }
+            return val;
+          });
+        }
+        return rowArr;
+      });
+      setBoard(newBoard);
+
+      // 게임 종료 여부 검사
+      if (checkWin(row, col)) {
+        setTimeout(() => {
+          alert(`${currentPlayer}가 승리하였습니다!`);
+        }, 0);
+        setCurrentPlayer(null);
+        // 게임 종료 후 필요한 작업 수행
+      } else {
+        setCurrentPlayer(currentPlayer === 'player1' ? 'player2' : 'player1');
+      }
+    }
+  };
+
+  // size의 수만큼 칸을 생성해주는 함수
+  const renderRows = () => {
+    return board.map((rowArr, rowIndex) => (
+      <GameBoardRow key={rowIndex}>
+        {rowArr.map((val, colIndex) => (
+          <Square
+            key={`${rowIndex}-${colIndex}`}
+            value={val}
+            onClick={() => handleSquareClick(rowIndex, colIndex)}
+          />
+        ))}
+      </GameBoardRow>
+    ));
   }
 
   return (
--- src/pages/GamePage.jsx
+++ src/pages/GamePage.jsx
@@ -1,9 +1,55 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useSelector } from 'react-redux'
+import GameBoard from '../components/GameBoard';
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
 
 const GamePage = () => {
+  const gameBoardSize = useSelector(state => state.gameSettings.gameBoardSize);
+  const winCondition = useSelector(state => state.gameSettings.winCondition);
+  const player1Mark = useSelector(state => state.gameSettings.player1Mark);
+  const player2Mark = useSelector(state => state.gameSettings.player2Mark);
+  const player1MarkColor = useSelector(state => state.gameSettings.player1MarkColor);
+  const player2MarkColor = useSelector(state => state.gameSettings.player2MarkColor);
+  const startPlayer = useSelector(state => state.gameSettings.startPlayer);
+
+  const [currentPlayer, setCurrentPlayer] = useState(null);
+
+  // 시작 플레이어 선택에 따라 먼저 시작할 플레이어 정해주는 함수
+  useEffect(() => {
+    if(startPlayer === 'random') {
+      const randomplayer = Math.random() < 0.5 ? 'player1' : 'player2';
+      setCurrentPlayer(randomplayer);
+    } else {
+      setCurrentPlayer(startPlayer);
+    }
+  }, [startPlayer]);
+
   return (
-    <div>GamePage</div>
+    <div>
+      {gameBoardSize}
+      {winCondition}
+      {player1Mark}
+      {player2Mark}
+      {player1MarkColor}
+      {player2MarkColor}
+      {startPlayer}
+      {
+        currentPlayer ?
+          <STitle>현재 {currentPlayer}</STitle> :
+          <STitle>게임이 끝났습니다.</STitle>
+      }
+      <GameBoard size={gameBoardSize} currentPlayer={currentPlayer} player1Mark={player1Mark} player2Mark={player2Mark} setCurrentPlayer={setCurrentPlayer} winCondition={winCondition}/>
+      <SLink to='/'>홈으로 돌아가기</SLink>
+    </div>
   )
 }
 
 export default GamePage
+
+const STitle = styled.div`
+  font-size: 30px;
+`;
+
+const SLink = styled(Link)`
+`;
