31cb67648976111231efc7d1f12c60614134b35f
yunbiyomi <yunbiyomi@naver.com>
2024-03-24 17:23:59 +0900

    ✨ Feat : 각 플레이어별 마크 색상 표시 기능 구현

--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 import styled from 'styled-components'
 
-const Square = ({ value }) => {
+const Square = ({ value, onClick }) => {
+  const player1Mark = useSelector(state => state.gameSettings.player1Mark);
+  const player1MarkColor = useSelector(state => state.gameSettings.player1MarkColor);
+  const player2MarkColor = useSelector(state => state.gameSettings.player2MarkColor);
+
+  // 플레이어에 따른 마크 색 변경
+  const markColor = value === player1Mark ? player1MarkColor : player2MarkColor;
+
   return (
-    <SquareBox>
+    <SquareBox onClick={onClick} color={markColor}>
       {value}
     </SquareBox>
   )
@@ -15,5 +23,11 @@ const SquareBox = styled.div`
   width: 100px;
   height: 100px;
   border: 5px solid black;
+  color: ${props => props.color};
+  box-sizing: border-box;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 24px;
   cursor: pointer;
 `;
